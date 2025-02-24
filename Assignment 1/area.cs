09b1487b5f4d56770c1d28b2ed1631a44e3e2db7
Karan Gupta <karanguptak.g4567@gmail.com>
2025-02-24 11:02:30 +0530

    Assignment1 Done

--- Assignment 1/area.cs
+++ Assignment 1/area.cs
@@ -1,12 +1,12 @@
 using System;
 
-class Program
+class CircleArea
 {
     static void Main(string[] args)
     {
         Console.Write("Enter the radius of the circle: ");
-        double radius = Convert.ToDouble(Console.ReadLine());
+        double radius = double.Parse(Console.ReadLine());
         double area = Math.PI * Math.Pow(radius, 2);
-        Console.WriteLine("The area of the circle is: {0:F2}", area);
+        Console.WriteLine($"The area of the circle with radius {radius} is: {area}");
     }
 }
--- Assignment 1/perimeter.cs
+++ Assignment 1/perimeter.cs
@@ -1,14 +1,14 @@
 using System;
 
-class Program
+class RectanglePerimeter
 {
     static void Main(string[] args)
     {
         Console.Write("Enter the length of the rectangle: ");
-        double length = Convert.ToDouble(Console.ReadLine());
+        double length = double.Parse(Console.ReadLine());
         Console.Write("Enter the width of the rectangle: ");
-        double width = Convert.ToDouble(Console.ReadLine());
+        double width = double.Parse(Console.ReadLine());
         double perimeter = 2 * (length + width);
-        Console.WriteLine("The perimeter of the rectangle is: {0:F2}", perimeter);
+        Console.WriteLine($"The perimeter of the rectangle is: {perimeter}");
     }
 }
